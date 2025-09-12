310c0dd9d813435d9ca8152a325efdcca0bd0855
jackson <jackson.dobson2026@gbwl.org>
2025-09-12 13:39:46 +0000

    save and commit changes

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
-def main():
-    print("hello world")
+print("hello world")
 
+lucky_num = 13 #int
+my_name = "jackson" #str
+cash = 5.50 #float
+is_raining = True #boolean
 
-if __name__ == "__main__":
-    main()
+greeting = "Hello, " + my_name
+print(greeting)
+# You can use single quotes instead
+greeting = 'Python is cool, it\'s better than Java'
+print(greeting)
+long_greeting = """
+                we were both young 
+                when i first saw you
+                i close my eyes
+                """
+print(long_greeting)
+
+#f-strings are FORMATTED strings (like templates)
+f_string = f"My name is {my_name} and my lucky number is {lucky_num}"
+print(f_string)
+
+# FUNCTIONS are reusable recipes/processes
+# Use the keyword def to define a new function
+def helloworld():
+    # function BODY indente under the colon
+    print("hello world I am a function")
+
+#UN-indent to signal the end of the block
+# CALL a void function
+helloworld()
+
+def multiply(x, y):
+    result = x*y
+    return result
+
+five_times_three = multiply(5,3)
+print(five_times_three)
+print(multiply(67,41))
